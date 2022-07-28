643f27abbe73cca3a0b35b738b26e0f22a44ef0c
sangsua <quang1994@web.de>
2022-07-28 02:35:47 +0200

    setup basic configurations

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#if !defined(CONFIG_H) 
+#define CONFIG_H
+// Will be used in chip8.h
+// Define Chip8_Memory_size
+#define CONFIG_EMULATOR_WINDOW_TITLE "Chip8 Emulator"
+#define CONFIG_CHIP8_MEMORY_SIZE 4096
+#define CONFIG_CHIP8_WIDTH 64
+#define CONFIG_CHIP8_HEIGHT 32
+#define CONFIG_CHIP8_WINDOW_MULTIPLIER 10
+
+
+#endif // CONFIG_H
--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include "SDL2/SDL.h"
+#include "chip8.h"
 int main(int argc, char ** argv)
 {
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* window = SDL_CreateWindow(
-        "CHIP8 WINDOW",
+        CONFIG_EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        640, 320, SDL_WINDOW_SHOWN
+        CONFIG_CHIP8_WIDTH * CONFIG_CHIP8_WINDOW_MULTIPLIER, 
+        CONFIG_CHIP8_HEIGHT * CONFIG_CHIP8_WINDOW_MULTIPLIER,
+        SDL_WINDOW_SHOWN
     );
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
